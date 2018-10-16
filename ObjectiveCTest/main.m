890a88a814db0a3b9b0cc77166aca71f8062b495
RickWang <woshiwwy16@126.com>
2018-10-16 16:36:22 +0800

    add loop

--- ObjectiveCTest/main.m
+++ ObjectiveCTest/main.m
@@ -13,6 +13,24 @@
 //一个程序的主函数，也是程序的入口
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        // For 循环
+        for (int a = 0; a < 10; a ++) {
+            NSLog(@"now a is %d", a);
+        }
+        
+        // While 循环
+        
+        int a = 12;
+        while (a > 0) {
+            NSLog(@"now a in while loop is %d", a);
+            a--;
+        }
+        
+        // Do While 循环,起码要执行一次
+        int b = 0;
+        do {
+            NSLog(@"now, b is %d", b);
+        } while(b > 0);
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
