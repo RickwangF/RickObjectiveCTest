cecf5d1f530da9c49a52d642961def411ecb5e97
RickWang <woshiwwy16@126.com>
2018-10-17 11:02:25 +0800

    add object

--- ObjectiveCTest/main.m
+++ ObjectiveCTest/main.m
@@ -9,10 +9,18 @@
 // #import是预处理命令，告诉编译器在编译之前需要引入以下的头文件
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Member.h"
 
 //一个程序的主函数，也是程序的入口
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        Member *member = [[Member alloc] init];
+        member.name = @"Rick";
+        NSLog(@"member's name is %@", member.name);
+        
+        member.age = 20;
+        NSLog(@"member's age is %d", member.age);
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
