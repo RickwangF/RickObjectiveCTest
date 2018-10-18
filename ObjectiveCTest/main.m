e28a56d438aaa2844410e8e1003bf794f303ac49
RickWang <woshiwwy16@126.com>
2018-10-18 10:42:27 +0800

    add block

--- ObjectiveCTest/main.m
+++ ObjectiveCTest/main.m
@@ -10,9 +10,49 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+void exampleB_addBlockToArray(NSMutableArray *array) {
+    char b = 'B';
+    [array addObject:^{
+        printf("%c\n", b);
+    }];
+}
+
+void exampleB() {
+    NSMutableArray *array = [NSMutableArray array];
+    exampleB_addBlockToArray(array);
+    // NSConcreteMallocBlock
+    void (^block)() = [array objectAtIndex:0];
+    block();
+}
+
 //一个程序的主函数，也是程序的入口
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        int (^myBlock) (int) = ^(int number){
+            return number * number;
+        };
+        NSLog(@"call myBlock's result is %d", myBlock(10));
+        
+        
+        __block NSString *string = @"Rick";
+        NSLog(@"string is %@", string);
+
+        // 捕获外部变量string
+        void(^block) (void) = ^() {
+            NSLog(@"string is %@, from block", string);
+        };
+
+        string = @"Smith";
+        NSLog(@"string is %@", string);
+        block();
+        
+        // NSConcreteGlobalBlock
+        ^{NSLog(@"hello world");}();
+        
+        // NSConcreteStackBlock
+        int a = 1;
+        ^{NSLog(@"print a is %d", a);}();
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
