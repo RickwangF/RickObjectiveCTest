88f91295ac2af24ea4f7250cfa77ce52bf5b56ac
RickWang <woshiwwy16@126.com>
2018-11-14 14:58:40 +0800

    add custom view

--- ObjectiveCTest/PushedThreeViewController.m
+++ ObjectiveCTest/PushedThreeViewController.m
@@ -8,48 +8,79 @@
 
 #import "PushedThreeViewController.h"
 #import "LeftImageButton.h"
+#import "RightImageButton.h"
 
 @interface PushedThreeViewController ()
 
-@property (nonatomic) LeftImageButton *customBtn;
+@property (nonatomic) LeftImageButton *leftBtn;
 
--(void) addBtnClicked:(UIButton*)sender;
+@property (nonatomic) RightImageButton *rightBtn;
 
 -(void) testBtnClicked:(UIButton*)sender;
+
+-(void) leftBtnClicked:(UIButton*)sender;
+
+-(void) rightBtnClicked:(UIButton*)sender;
 
 @end
 
 @implementation PushedThreeViewController
 
-@synthesize customBtn;
+@synthesize leftBtn, rightBtn;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 35)];
+    containerView.layer.borderColor = [[UIColor redColor] CGColor];
+    containerView.layer.borderWidth = 1.0f;
+    UIButton *testBtn = [[UIButton alloc] initWithFrame:CGRectMake(-16, 0, 100, 35)];
+    testBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [testBtn setTitle:@"左侧按钮" forState:UIControlStateNormal];
+    [testBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [testBtn addTarget:self action:@selector(testBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [containerView addSubview:testBtn];
+    UIBarButtonItem *leftBarItem = [[UIBarButtonItem alloc] initWithCustomView:containerView];
+    self.navigationItem.leftBarButtonItem = leftBarItem;
+
     
-    customBtn = [[LeftImageButton alloc] initWithFrame:CGRectMake(0, 0, 100, 35)];
-    [customBtn addTarget:self action:@selector(testBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
-    [customBtn setTitle:@"测试按钮" forState:UIControlStateNormal];
-    [customBtn setImage:[UIImage imageNamed:@"add_25#blue"] forState: UIControlStateNormal];
-    [customBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    customBtn.titleLabel.font = [UIFont systemFontOfSize:14];
-    [customBtn sizeToFit];
-    customBtn.translatesAutoresizingMaskIntoConstraints = NO;
+//    leftBtn = [[LeftImageButton alloc] initWithFrame:CGRectMake(0, 0, 100, 35)];
+//    [leftBtn addTarget:self action:@selector(leftBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+//    [leftBtn setTitle:@"左侧按钮" forState:UIControlStateNormal];
+//    [leftBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+//    // 这个是关键，因为UINavigationBar中使用了AutoLayout，不需要frame等信息决定约束
+//    leftBtn.translatesAutoresizingMaskIntoConstraints = NO;
+//
+//    UIBarButtonItem *leftBarItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
+//    UIBarButtonItem *fixedSpaceItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+//    fixedSpaceItem.width = 6;
+//
+//    self.navigationItem.leftBarButtonItems = @[fixedSpaceItem, leftBarItem];
     
-    UIBarButtonItem *leftBarItem = [[UIBarButtonItem alloc] initWithCustomView:customBtn];
-    UIBarButtonItem *fixedSpaceItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
-    fixedSpaceItem.width = 6;
     
-    self.navigationItem.leftBarButtonItems = @[fixedSpaceItem, leftBarItem];
-    // Do any additional setup after loading the view.
+    
+//    UIButton *testBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 35)];
+//    testBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+//    [testBtn setTitle:@"左侧按钮" forState:UIControlStateNormal];
+//    [testBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+//    UIBarButtonItem *testBarItem = [[UIBarButtonItem alloc] initWithCustomView:testBtn];
+//
+//    UIBarButtonItem *fixedSpaceItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+//        fixedSpaceItem.width = 6;
+//
+//    self.navigationItem.leftBarButtonItems = @[fixedSpaceItem, testBarItem];
 }
 
-- (void)addBtnClicked:(UIButton *)sender{
-    NSLog(@"add Btn Clicked");
+- (void)testBtnClicked:(UIButton *)sender{
+    NSLog(@"test btn clicked");
 }
 
--(void)testBtnClicked:(UIButton *)sender{
-    NSLog(@"test Btn Clicked");
+-(void)leftBtnClicked:(UIButton *)sender{
+    NSLog(@"left btn clicked");
+}
+
+-(void)rightBtnClicked:(UIButton *)sender{
+    NSLog(@"right btn clicked");
 }
 
 /*
