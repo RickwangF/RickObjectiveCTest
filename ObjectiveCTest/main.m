ace99384748de8db8be565c104da5876a069c394
RickWang <woshiwwy16@126.com>
2018-11-20 14:37:50 +0800

    add struct array

--- ObjectiveCTest/main.m
+++ ObjectiveCTest/main.m
@@ -10,34 +10,47 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 #import "User.h"
+#import <CoreLocation/CoreLocation.h>
 
 //一个程序的主函数，也是程序的入口
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         
-        NSArray<User*> *array = @[[[User alloc] init], [[User alloc] init]];
-        for (User* user in array) {
-            NSLog(@"foreach user's name is %@", user.UserName);
-        }
+        CLLocationCoordinate2D coordinates[10];
         
-        NSArray<User*> *userArray = [NSArray arrayWithObjects: [[User alloc] init], [[User alloc] init], [[User alloc] init], nil];
-        for (User* user in userArray) {
-            user.UserAge += 1;
-            NSLog(@"foreach user's age is %d", user.UserAge);
-        }
+        CLLocationCoordinate2D coor[10] = {0};
+        NSLog(@"coor[0] latitude is %f and longtitude is %f", coor[0].latitude, coor[0].longitude);
         
-        NSArray *genericArray = [NSArray arrayWithArray: array];
-        for (User* user in genericArray) {
-            NSLog(@"foreach user's age is %d", user.UserAge);
-        }
+        int count = 10;
+        CLLocationCoordinate2D array[count];
         
-        NSString *name = [array objectAtIndex:0].UserName;
-        NSLog(@"foreach name's age is %@", name);
+        array[0].latitude = 121.0;
+        array[0].longitude = 122.0;
+        NSLog(@"array[0] latitude is %f and longtitude is %f", array[0].latitude, array[0].longitude);
         
-        // 定义数字型数组,不能使用基本的值类型
-        NSArray<NSNumber*> *intArray = @[@123, @12345];
-        
-        NSArray *numArray = @[@123.4, @1234];
+//        NSArray<User*> *array = @[[[User alloc] init], [[User alloc] init]];
+//        for (User* user in array) {
+//            NSLog(@"foreach user's name is %@", user.UserName);
+//        }
+//
+//        NSArray<User*> *userArray = [NSArray arrayWithObjects: [[User alloc] init], [[User alloc] init], [[User alloc] init], nil];
+//        for (User* user in userArray) {
+//            user.UserAge += 1;
+//            NSLog(@"foreach user's age is %d", user.UserAge);
+//        }
+//
+//        NSArray *genericArray = [NSArray arrayWithArray: array];
+//        for (User* user in genericArray) {
+//            NSLog(@"foreach user's age is %d", user.UserAge);
+//        }
+//
+//        NSString *name = [array objectAtIndex:0].UserName;
+//        NSLog(@"foreach name's age is %@", name);
+//
+//        // 定义数字型数组,不能使用基本的值类型
+//        NSArray<NSNumber*> *intArray = @[@123, @12345];
+//
+//        NSArray *numArray = @[@123.4, @1234];
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
